0d9efee50ac42e61accb5baa940b86a0bc7214b8
AddingHafidz <123148414+AddingHafidz@users.noreply.github.com>
2023-01-20 16:52:16 +0700

    Add files via upload

---
+++ uaspakharry/no1/halaman/page1.php
@@ -0,0 +1,3 @@
+
+<p style="color: black; font-size: 25px; background-color:white;">PT. BIOGAS MURNI bergerak di bidang kopi, di mana kopi dari PT kami di olah lagi sampai jadi kopi racikan terbaik.
+      </p>
